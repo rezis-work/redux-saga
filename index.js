ac73eeca583180982ed4579dca9a77a3377870b2
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-02-01 16:11:53 +0400

    implement compose function from ground up

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ import {
   bindActionCreators,
   combineReducers,
 } from "redux";
+
+const makeLouder = (string) => string.toUpperCase();
+
+const repeatthreeTimes = (string) => string.repeat(3);
+
+const embolden = (string) => string.bold();
+
+// const makeLouderThreeTimesAndEmbolden = (string) =>
+//   embolden(repeatthreeTimes(makeLouder(string)));
+
+const makeLouderThreeTimesAndEmbolden = compose(
+  embolden,
+  repeatthreeTimes,
+  makeLouder
+);
+
+console.log(makeLouderThreeTimesAndEmbolden("hello"));
